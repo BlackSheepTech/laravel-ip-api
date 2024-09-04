4e932e2a95ea33a31e136cb80295f852f5ab3f7a
Israel Pinheiro <israel.pinheiro@live.com>
2024-09-04 12:15:58 -0300

    chore: Implement Batch API

--- src/IpApiBatch.php
+++ src/IpApiBatch.php
@@ -2,10 +2,100 @@
 
 namespace BlackSheepTech\IpApi;
 
+use BlackSheepTech\IpApi\Enums\HttpMethod;
+use BlackSheepTech\IpApi\Enums\ReturnField as Fields;
+use BlackSheepTech\IpApi\Enums\ReturnLanguage as Languages;
+use Illuminate\Support\Str;
+
 class IpApiBatch extends IpApi
 {
+    private array $entities;
+
+    private string $language;
+
     public function __construct()
     {
-        //
+        $this->baseUrl(config('ip-api.base_url'));
+        $this->apiKey(config('ip-api.api_key'));
+        $this->language(config('ip-api.default.language'));
+    }
+
+    public function entities(array $entities): self
+    {
+        throw_if(count($entities) > config('ip-api.batch.max_entities'), new \InvalidArgumentException('Maximum number of entities per request exceeded.'));
+        collect($entities)->each(fn ($entity) => $this->validateEntity($entity));
+
+        $this->entities = $entities;
+
+        return $this;
+    }
+
+    public function language(string $language): self
+    {
+        throw_unless(
+            in_array($language, Languages::values()),
+            new \InvalidArgumentException('Invalid language provided.')
+        );
+
+        $this->language = $language;
+
+        return $this;
+    }
+
+    private function validateEntity(object $entity): void
+    {
+        throw_unless(
+            Validators::isValidDomain($entity->query) || Validators::isValidIpAddress($entity->query),
+            new \InvalidArgumentException('Query must be a valid domain or IP address.')
+        );
+
+        throw_unless(
+            in_array($entity->lang, Languages::values()),
+            new \InvalidArgumentException('Invalid language provided.')
+        );
+
+        throw_unless(
+            array_diff(is_array($entity->fields) ? $entity->fields : explode(',', Str::remove(' ', $entity->fields)), Fields::values()) === [],
+            new \InvalidArgumentException('Invalid fields provided.')
+        );
+    }
+
+    public function get(bool $asObject = false): array|object|string
+    {
+        $this->validateParams();
+
+        $this->blockOverusage();
+
+        $payload = [
+            'key' => $this->apiKey,
+            'lang' => $this->language,
+            'data' => $this->entities,
+        ];
+
+        $response = HttpHelper::makeRequest(
+            method: HttpMethod::POST(),
+            baseUrl: $this->baseUrl,
+            endpoint: 'batch',
+            payload: $payload,
+        );
+
+        $this->trackUsage($response->header('X-Rl'), $response->header('X-Ttl'));
+
+        if ($asObject) {
+            return (object) $response->json();
+        }
+
+        return $response->json();
+    }
+
+    public function getAsObject(): object
+    {
+        return $this->get(true);
+    }
+
+    private function validateParams(): void
+    {
+        throw_unless(isset($this->entities), new \InvalidArgumentException('Entities must be provided.'));
+        throw_if(empty($this->apiKey), new \InvalidArgumentException('API Key must be provided.'));
     }
 }
